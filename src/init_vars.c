6319fbccd8685cd57b416ab7c4106ad520c1eb7c
keyiflerolsun <keyiflerolsun@gmail.com>
2025-07-23 22:20:25 +0300

    Add init_vars function to initialize input and output file descriptors

---
+++ src/init_vars.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_vars.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osancak <osancak@student.42istanbul.com.tr +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 09:38:01 by osancak           #+#    #+#             */
+/*   Updated: 2025/07/23 18:28:14 by osancak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	init_vars(t_vars *vars, char *input_file, char *output_file)
+{
+	vars->infile = open(input_file, O_RDONLY);
+	if (vars->infile < 0)
+		error_exit(input_file, 42);
+	vars->outfile = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (vars->outfile < 0)
+		error_exit(output_file, 42);
+	vars->last_read = -1;
+}
