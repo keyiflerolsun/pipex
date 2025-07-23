735549d9dd8496a929e9ecc3d6009bbe7f699c7c
keyiflerolsun <keyiflerolsun@gmail.com>
2025-07-23 22:58:33 +0300

    init_vars: change infile and outfile to use O_RDWR for /dev/null

--- src/init_vars.c
+++ src/init_vars.c
@@ -17,13 +17,16 @@ void	init_vars(t_vars *vars, char *input_file, char *output_file)
 	vars->infile = open(input_file, O_RDONLY);
 	if (vars->infile < 0)
 	{
-		vars->infile = open("/dev/null", O_CREAT | O_WRONLY | O_TRUNC);
+		vars->infile = open("/dev/null", O_CREAT | O_RDWR | O_TRUNC);
 		error_exit(input_file, 42);
 	}
-	vars->outfile = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (ft_strstr(input_file, "here_doc"))
+		vars->outfile = open(output_file, O_CREAT | O_WRONLY | O_APPEND, 0644);
+	else
+		vars->outfile = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (vars->outfile < 0)
 	{
-		vars->infile = open("/dev/null", O_CREAT | O_WRONLY | O_TRUNC);
+		vars->infile = open("/dev/null", O_CREAT | O_RDWR | O_TRUNC);
 		error_exit(output_file, 42);
 	}
 	vars->last_read = -1;
