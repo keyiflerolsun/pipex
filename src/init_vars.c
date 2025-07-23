3085f9f089f066779c6e18f50948b6af383974d9
keyiflerolsun <keyiflerolsun@gmail.com>
2025-07-23 22:47:00 +0300

    init_vars: redirect infile and outfile to /dev/null on open failure

--- src/init_vars.c
+++ src/init_vars.c
@@ -16,9 +16,15 @@ void	init_vars(t_vars *vars, char *input_file, char *output_file)
 {
 	vars->infile = open(input_file, O_RDONLY);
 	if (vars->infile < 0)
+	{
+		vars->infile = open("/dev/null", O_CREAT | O_WRONLY | O_TRUNC);
 		error_exit(input_file, 42);
+	}
 	vars->outfile = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (vars->outfile < 0)
+	{
+		vars->infile = open("/dev/null", O_CREAT | O_WRONLY | O_TRUNC);
 		error_exit(output_file, 42);
+	}
 	vars->last_read = -1;
 }
